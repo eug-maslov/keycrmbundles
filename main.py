fbccb3e514dc646680966e796baa64af46c1743f
eug-maslov <evv.maslov@gmail.com>
2025-04-14 19:04:18 +0300

    Setup basic code construction

---
+++ main.py
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+def main():
+
+
+
+    order_id = 71588
+
+
+
+
+    order_data = get_order_data(order_id)
+
+    order_products = order_data['products']
+
+    bundles = []
+
+    for product in order_products:
+        if "[bundle]" in product.name:
+            bundles.append(product)
+
+    
+    for bundle in bundles:
+
+        bundle_data = get_product_data_by_sku(bundle.sku)
+
+        bundle_custom_field = bundle_data[BUNDLE_CUSTOM_FIELD_ID]
+
+        if bundle_custom_field != None:
+            
+
+            products_skus = get_products_skus_from_bundle_custom_field(bundle_custom_field)
+
+            add_products_to_order_by_skus(products_skus)
+
+            
+
+
+
+            
+
